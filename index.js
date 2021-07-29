c5e6b31c51ab693775638a6d0e724c03c73d6b54
Zane Wolfgang Pickett <sirwolfgang@users.noreply.github.com>
2021-07-29 11:01:08 -0500

    Test

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const timezone = core.getInput('timezone')
 const now = moment().tz(timezone)
 
 if (now.day() === 5 || now.day() === 6 || now.day() === 7) {
+  console.log('Test')
   core.setFailed('It\'s probably better if you leave it for next week')
 }
