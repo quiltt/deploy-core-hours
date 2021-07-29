e2dcdc26d6c3128f977975ec1a5dca555c2b3d43
Zane Wolfgang Pickett <sirwolfgang@users.noreply.github.com>
2021-07-29 12:42:11 -0500

    Rewrite for holidays and business days

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+import { DateTime } from 'luxon-business-days'
+
 const core = require('@actions/core')
-const moment = require('moment-timezone')
 
-const timezone = core.getInput('timezone')
+const timezone     = core.getInput('timezone')
+const dayStartHour = core.getInput('dayStartHour')
+const dayEndHour   = core.getInput('dayEndHour')
+const deployDays   = [
+  core.getInput('deployMonday')    && 1,
+  core.getInput('deployTuesday')   && 2,
+  core.getInput('deployWednesday') && 3,
+  core.getInput('deployThursday')  && 4,
+  core.getInput('deployFriday')    && 5
+].filter(x => x)
 
-const now = moment().tz(timezone)
+const currentTime = DateTime.now().setZone(timezone).setupBusiness(
+  { businessDays: deployDays }
+)
 
-if (now.day() === 5 || now.day() === 6 || now.day() === 7) {
+if(
+  !currentTime.isBusinessDay() ||
+  currentTime.isHoliday() ||
+  currentTime.hour() <= dayStartHour ||
+  currentTime.hour() >= dayEndHour
+) {
   core.setFailed('It\'s probably better if you leave it for next week')
 }
